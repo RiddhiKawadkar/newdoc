7a0c9cf41116dd8ccbf805c1627dde7968c5254d
Riddhi Kawadkar <riddhikawadkar421@gmail.com>
2023-07-12 18:58:52 +0530

    added new file

---
+++ src/riddhiProject/studyPad.java
@@ -0,0 +1,12 @@
+package riddhiProject;
+
+public class studyPad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello");
+System.out.println("riddhi");
+	}
+
+}
